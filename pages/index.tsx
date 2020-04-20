c5489b77c8fec1315fd3083121e40636c03bdaa3
Darash Desai <darash@lyvewave.com>
2020-04-20 16:28:09 -0400

    Implement parsing of IHME data and displaying it on the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,13 @@ import { NextPage } from "next";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import { FormControlProps } from "react-bootstrap/FormControl";
-import IHMEParser from "../components/IHMEParser";
+import IHMEParser, { IHMEParserProps } from "../components/IHMEParser";
 import Row from "react-bootstrap/Row";
 import StateDropdown from "../components/StateDropdown";
 import CTPPlot, { CTPDataPoint } from "../components/CTPPlot";
+
+import { Line } from "react-chartjs-2";
+import { ChartPoint, TimeUnit } from "chart.js";
 
 import fetch from "../lib/fetch";
 
@@ -31,6 +34,9 @@ const fetchCovidData = async (
 const Index: NextPage = () => {
   const [data, setData] = useState<CTPDataPoint[] | null>(null);
 
+  type ChartData = { [key: string]: ChartPoint[] };
+  const [chartData, setChartData] = useState<ChartData>({});
+
   const onChange: FormControlProps["onChange"] = async (
     event
   ): Promise<void> => {
@@ -38,6 +44,34 @@ const Index: NextPage = () => {
     const results = await fetchCovidData(state);
 
     setData(results);
+  };
+
+  const onIHMEUpdate: IHMEParserProps["onUpdate"] = (data) => {
+    setChartData(data);
+  };
+
+  const lineData = {
+    datasets: Object.keys(chartData)
+      .sort()
+      .map((key) => {
+        return {
+          label: key,
+          data: chartData[key],
+        };
+      }),
+  };
+
+  const options = {
+    scales: {
+      xAxes: [
+        {
+          type: "time",
+          time: {
+            unit: "day" as TimeUnit,
+          },
+        },
+      ],
+    },
   };
 
   return (
@@ -49,11 +83,16 @@ const Index: NextPage = () => {
       </Row>
       <Row>
         <Col>
-          <IHMEParser />
+          <IHMEParser onUpdate={onIHMEUpdate} />
         </Col>
       </Row>
       <Row>
         <Col>{data && <CTPPlot data={data} />}</Col>
+      </Row>
+      <Row>
+        <Col>
+          <Line data={lineData} options={options}></Line>
+        </Col>
       </Row>
     </Container>
   );
